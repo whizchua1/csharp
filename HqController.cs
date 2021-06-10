743ae3bd8fb9be91702f2c86cb5d9754b3d9c66e
whizchua1 <whiz.chua@gmail.com>
2021-06-10 17:34:45 +0800

    Add files via upload

---
+++ HqController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using www.Models;
+
+namespace www.HqControllerControllers
+{
+    public class HqController : Controller
+    {
+        IDistributedCache _redis = null;
+        public HqController(IDistributedCache cache)
+        {
+            _redis = cache;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            var key = "aspnet:{Staff}:bio";
+            var bio = _redis.GetString(key);
+            if (string.IsNullOrWhiteSpace(bio))
+            {
+                bio = "John"; // here we call "expensive" or "slow" web service...
+
+                _redis.SetString(key, bio);
+            }
+
+            HttpContext.Session.Set("HqSessionKey",
+                Encoding.UTF8.GetBytes("Redis session in progress"));
+
+            ViewData["Message"] = $"About Staff {bio + " "}";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Retrieving Contact Details....";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
